e724a2010736e2760708c190dfa460afd5e675d4
Giovanni Gómez Restrepo <giovagr20@users.noreply.github.com>
2019-10-26 13:14:15 -0500

    Primer api

--- server/controllers/controllers.products.js
+++ server/controllers/controllers.products.js
@@ -2,6 +2,10 @@ const _productController = {};
 
 const Products = require('../models/models.products');
 
+_productController.getProducts = async (req, res)=>{
+    const products = await Products.find();
+    res.json(products);
+};
 
 
 module.exports = _productController;
--- server/routes/routes.products.js
+++ server/routes/routes.products.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const _productsController = require('../controllers/controllers.products');
 
+router.get('/', _productsController.getProducts) //Products in database
 
 
 
